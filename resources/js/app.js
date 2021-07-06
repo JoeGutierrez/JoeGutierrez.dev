653079a2823075c7884ffee37658521206ffaad2
root <root@joeswebserver.joegutierrez.ninja>
2021-07-05 19:07:00 -0700

    Deleted my portfolio that used the Bootstrap 5 Landkit theme.

--- resources/js/app.js
+++
@@ -1,6 +0,0 @@
-require( './bootstrap' );
-
-// Landkit Bootstrap theme. Added: 10/18/2020.
-require( './landkit/mapbox-gl' );
-require( './landkit/vendor.bundle' );
-require( './landkit/theme.bundle' );
--- resources/views/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends( 'layouts.app' )
-
-@section( 'content' )
-
-<h1>Welcome to the Test Home Page</h1>
-
-@endsection
