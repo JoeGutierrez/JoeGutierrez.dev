30ea34298ece505bbdc6fdf20094a5d6ca077177
root <root@joeswebserver.joegutierrez.ninja>
2021-07-06 00:39:47 -0700

    Finished the "My Services" section and the footer, updated the favicon.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 		<meta content="" name="keywords">
 
 		<!-- Favicons -->
-		<link href="/img/favicon.png" rel="icon">
+		<link href="/img/favicon.ico" rel="icon">
 		<link href="/img/apple-touch-icon.png" rel="apple-touch-icon">
 
 		<!-- Google Fonts -->
@@ -78,6 +78,7 @@
 		<!-- ======= Footer ======= -->
 		<footer id="footer">
 			<div class="container">
+<?php /*
 				<h3>JoeGutierrez.dev</h3>
 				<p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
 				<div class="social-links">
@@ -87,18 +88,10 @@
 					<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
 					<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
 				</div>
+*/ ?>
 				<div class="copyright">
 					&copy;{{ now()->year }} Copyright <?php //<strong><span> ?>JoeGutierrez.dev<?php //</span></strong> ?>. All Rights Reserved.
 				</div>
-<?php /*
-				<div class="credits">
-					<!-- All the links in the footer should remain intact. -->
-					<!-- You can delete the links only if you purchased the pro version. -->
-					<!-- Licensing information: [license-url] -->
-					<!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/ -->
-					Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-				</div>
-*/ ?>
 			</div>
 		</footer><!-- End Footer -->
 
